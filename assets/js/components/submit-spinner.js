14170ef0ce235a91312c6655861ddb8c2c88cea4
munechika-koyo <munechika.koyo@torus.nr.titech.ac.jp>
2024-04-09 16:29:09 +0900

    ♻️ Refactor submit-spinner.js to use ES6 syntax

--- assets/js/components/submit-spinner.js
+++ assets/js/components/submit-spinner.js
@@ -1,27 +1,33 @@
 /**
- * submit buttonが押されたら、spinnerを表示する
- * @param {string} buttonID - type="submit"のボタンID
- * @return {boolean} - true を返す。これがないとフォームが送信されない
- * @example
- * <form
-    action=""
-    method="POST"
-    target="hidden_iframe"
-    onsubmit="submittingSpinner('submitButton'); submitted=true;"
-  >
-  ...
-    <button type="submit" id="submitButton">送信</button>
-  </form>
-*/
-function submittingSpinner(buttonID) {
-  // ボタン要素を取得
-  const button = document.getElementById(buttonID);
-  // spinnerを表示
-  button.innerHTML =
-    '<span class="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"></span>送信中...';
-  // ボタンをdisabledにする
-  button.disabled = true;
+ * Submit spinner animation.
+ * When the button is clicked, the spinner and the text "送信中..." are displayed on the button.
+ *
+ * Copyright 2024 プラズマ・核融合 若手フォーラム
+ */
 
-  // true を返すとフォームが正常に送信される
-  return true;
-}
+export default (() => {
+  const selector = "submit-spinner";
+
+  window.addEventListener(
+    "load",
+    () => {
+      // Fetch all the buttons we want to apply custom spinner styles to
+      const buttons = document.getElementsByClassName(selector);
+      // Loop over them and prevent submission
+      /* eslint-disable no-unused-vars */
+      const spinner = Array.prototype.filter.call(buttons, (button) => {
+        button.addEventListener(
+          "click",
+          () => {
+            button.innerHTML =
+              '<span class="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"></span>送信中...';
+            button.disabled = true;
+          },
+          false
+        );
+      });
+      /* eslint-enable no-unused-vars */
+    },
+    false
+  );
+})();
